f62acd67af8cdf9ff4c29410aae5bbe8030458dd
creecros <creecros@gmail.com>
2018-04-12 16:15:23 -0400

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -4,14 +4,19 @@ namespace Kanboard\Plugin\Group_assign;
 
 use Kanboard\Core\Plugin\Base;
 use Kanboard\Model\TaskModel;
+use Kanboard\Model\ProjectGroupRoleModel;
 use Kanboard\Plugin\Group_assign\Model\NewTaskFinderModel;
 use Kanboard\Plugin\Group_assign\Helper\NewTaskHelper;
+use Kanboard\Plugin\Group_assign\Controller\TaskCreationController;
 use PicoDb\Table;
 
 class Plugin extends Base
 {
     public function initialize()
     {
+        
+        $groups = $this->projectGroupRoleModel->getGroups($project['id']);
+
         //Helpers
         $this->helper->register('newTaskHelper', '\Kanboard\Plugin\Group_assign\Helper\NewTaskHelper');
         
