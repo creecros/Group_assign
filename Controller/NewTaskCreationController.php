a1b61b378a1d12312170affb6bdb8739547e9e45
creecros <creecros@gmail.com>
2018-04-12 16:00:29 -0400

    Update NewTaskCreationController.php

--- Controller/NewTaskCreationController.php
+++ Controller/NewTaskCreationController.php
@@ -2,6 +2,11 @@
 
 namespace namespace Kanboard\Plugin\Group_assign\Controller;
 
+use Kanboard\Model\ProjectGroupRoleModel;
+use Kanboard\Model\SwimlaneModel;
+use Kanboard\Model\ColumnModel;
+use Kanboard\Model\ProjectUserRoleModel;
+use Kanboard\Model\CategoryModel;
 use Kanboard\Core\Controller\PageNotFoundException;
 
 /**
@@ -10,7 +15,7 @@ use Kanboard\Core\Controller\PageNotFoundException;
  * @package  Kanboard\Controller
  * @author   Frederic Guillot
  */
-class NewTaskCreationController extends BaseController
+class NewTaskCreationController extends \Kanboard\Controller\TaskCreationController
 {
     /**
      * Display a form to create a new task
@@ -41,122 +46,4 @@ class NewTaskCreationController extends BaseController
         )));
     }
 
-    /**
-     * Validate and save a new task
-     *
-     * @access public
-     */
-    public function save()
-    {
-        $project = $this->getProject();
-        $values = $this->request->getValues();
-        $values['project_id'] = $project['id'];
-
-        list($valid, $errors) = $this->taskValidator->validateCreation($values);
-
-        if (! $valid) {
-            $this->flash->failure(t('Unable to create your task.'));
-            $this->show($values, $errors);
-        } else if (! $this->helper->projectRole->canCreateTaskInColumn($project['id'], $values['column_id'])) {
-            $this->flash->failure(t('You cannot create tasks in this column.'));
-            $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => $project['id'])), true);
-        } else {
-            $task_id = $this->taskCreationModel->create($values);
-
-            if ($task_id > 0) {
-                $this->flash->success(t('Task created successfully.'));
-                $this->afterSave($project, $values, $task_id);
-            } else {
-                $this->flash->failure(t('Unable to create this task.'));
-                $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => $project['id'])), true);
-            }
-        }
-    }
-
-    /**
-     * Duplicate created tasks to multiple projects
-     *
-     * @throws PageNotFoundException
-     */
-    public function duplicateProjects()
-    {
-        $project = $this->getProject();
-        $values = $this->request->getValues();
-
-        if (isset($values['project_ids'])) {
-            foreach ($values['project_ids'] as $project_id) {
-                $this->taskProjectDuplicationModel->duplicateToProject($values['task_id'], $project_id);
-            }
-        }
-
-        $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => $project['id'])), true);
-    }
-
-    /**
-     * Executed after the task is saved
-     *
-     * @param array   $project
-     * @param array   $values
-     * @param integer $task_id
-     */
-    protected function afterSave(array $project, array &$values, $task_id)
-    {
-        if (isset($values['duplicate_multiple_projects']) && $values['duplicate_multiple_projects'] == 1) {
-            $this->chooseProjects($project, $task_id);
-        } elseif (isset($values['another_task']) && $values['another_task'] == 1) {
-            $this->show(array(
-                'owner_id' => $values['owner_id'],
-                'color_id' => $values['color_id'],
-                'category_id' => isset($values['category_id']) ? $values['category_id'] : 0,
-                'column_id' => $values['column_id'],
-                'swimlane_id' => isset($values['swimlane_id']) ? $values['swimlane_id'] : 0,
-                'another_task' => 1,
-            ));
-        } else {
-            $this->response->redirect($this->helper->url->to('BoardViewController', 'show', array('project_id' => $project['id'])), true);
-        }
-    }
-
-    /**
-     * Prepare form values
-     *
-     * @access protected
-     * @param  bool  $isPrivateProject
-     * @param  array $swimlanesList
-     * @return array
-     */
-    protected function prepareValues($isPrivateProject, array $swimlanesList)
-    {
-        $values = array(
-            'swimlane_id' => $this->request->getIntegerParam('swimlane_id', key($swimlanesList)),
-            'column_id'   => $this->request->getIntegerParam('column_id'),
-            'color_id'    => $this->colorModel->getDefaultColor(),
-        );
-
-        if ($isPrivateProject) {
-            $values['owner_id'] = $this->userSession->getId();
-        }
-
-        return $values;
-    }
-
-    /**
-     * Choose projects
-     *
-     * @param array $project
-     * @param integer $task_id
-     */
-    protected function chooseProjects(array $project, $task_id)
-    {
-        $task = $this->taskFinderModel->getById($task_id);
-        $projects = $this->projectUserRoleModel->getActiveProjectsByUser($this->userSession->getId());
-        unset($projects[$project['id']]);
-
-        $this->response->html($this->template->render('task_creation/duplicate_projects', array(
-            'project' => $project,
-            'task' => $task,
-            'projects_list' => $projects,
-            'values' => array('task_id' => $task['id'])
-        )));
-    }
 }
