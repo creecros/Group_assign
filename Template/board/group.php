a41a73447251fc55b21b2f2666181fc35c2db3d1
creecros <creecros@gmail.com>
2018-04-13 01:54:23 -0400

    Update group.php

--- Template/board/group.php
+++ Template/board/group.php
@@ -1,5 +1,6 @@
 <span>
 <?php if ($task['assigned_groupname']): ?>
+     <strong><?= t('Assigned Group:') ?></strong>
      <?= $this->text->e($task['assigned_groupname'] ?: $task['owner_gp']) ?>
 <?php endif ?>
 </span>
