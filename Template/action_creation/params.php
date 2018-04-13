de380becf8168527ac182d47c32e0388431a9972
creecros <creecros@gmail.com>
2018-04-13 11:33:04 -0400

    Update params.php

--- Template/action_creation/params.php
+++ Template/action_creation/params.php
@@ -22,16 +22,18 @@
             <?= $this->form->label($param_desc, $param_name) ?>
             <?= $this->form->select('params['.$param_name.']', $users_list, $values) ?>
         <?php elseif ($this->text->contains($param_name, 'group_id')): ?>
-            <?= $groups = $this->model->projectGroupRoleModel->getGroups('project_id') ?>
-            <?= $groupnames = array() ?>
-            <?= $groupids = array() ?>
-            <?= $groupids[] = 0 ?>
-            <?= $groupnames[] = t('Unassigned') ?>
-            <?php foreach ($groups as $group): ?>
-                 <?= $groupnames[] = $group['name'] ?>
-                 <?= $groupids[] = $group['id'] ?>
-            <?php endforeach ?>
-            <?= $groupvalues = array_combine($groupids, $groupnames) ?>
+           <?php 
+                 $groups = $this->model->projectGroupRoleModel->getGroups('project_id');
+                 $groupnames = array(); 
+                 $groupids = array(); 
+                 $groupids[] = 0; 
+                 $groupnames[] = t('Unassigned'); 
+                 foreach ($groups as $group) {
+                       $groupnames[] = $group['name']; 
+                       $groupids[] = $group['id']; 
+                 }
+                 $groupvalues = array_combine($groupids, $groupnames);
+            ?>
             <?= $this->form->label($param_desc, $param_name) ?>
             <?= $this->form->select('params['.$param_name.']', $groupvalues, $values) ?>
         <?php elseif ($this->text->contains($param_name, 'check_box')): ?>
