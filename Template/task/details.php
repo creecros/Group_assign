1ea427eee1a50495f5c75a82d80ad9b7c84944b5
creecros <creecros@gmail.com>
2018-04-12 21:12:23 -0400

    Update details.php

--- Template/task/details.php
+++ Template/task/details.php
@@ -90,7 +90,7 @@
                         <strong><?= t('Assigned Group:') ?></strong>
                         <span>
                         <?php if ($task['assigned_groupname']): ?>
-                            <?= $this->text->e($task['owner_gp'] ?: $task['assigned_groupname']) ?>
+                            <?= $this->text->e($task['assigned_groupname'] ?: $task['owner_gp']) ?>
                         <?php else: ?>
                             <?= t('not assigned') ?>
                         <?php endif ?>
