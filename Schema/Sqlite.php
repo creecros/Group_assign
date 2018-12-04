ae322298352a4ebadccc17cf8d904e545ab3fc92
Craig Crosby <creecros@gmail.com>
2018-12-04 12:45:00 -0500

    Update Sqlite.php

--- Schema/Sqlite.php
+++ Schema/Sqlite.php
@@ -4,7 +4,30 @@ namespace Kanboard\Plugin\group_assign\Schema;
 
 use PDO;
 
-const VERSION = 1;
+const VERSION = 2;
+
+function version_1(PDO $pdo)
+{
+    $pdo->exec("ALTER TABLE tasks ADD COLUMN owner_ms INTEGER DEFAULT '0'");
+    
+    $pdo->exec("
+        CREATE TABLE multiselect (
+            id INTEGER PRIMARY KEY,
+            external_id TEXT DEFAULT '',
+            name TEXT NOCASE NOT NULL UNIQUE
+        )
+    ");
+    
+    $pdo->exec("
+        CREATE TABLE multiselect_has_users (
+            group_id INTEGER NOT NULL,
+            user_id INTEGER NOT NULL,
+            FOREIGN KEY(group_id) REFERENCES groups(id) ON DELETE CASCADE,
+            FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+            UNIQUE(group_id, user_id)
+        )
+    ");
+}
 
 function version_1(PDO $pdo)
 {
