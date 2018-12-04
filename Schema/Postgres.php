a5ea6c18c5b96dd6b34f701a21149e012b815488
Craig Crosby <creecros@gmail.com>
2018-12-04 12:48:53 -0500

    Update Postgres.php

--- Schema/Postgres.php
+++ Schema/Postgres.php
@@ -8,5 +8,28 @@ const VERSION = 1;
 
 function version_1(PDO $pdo)
 {
+    $pdo->exec("ALTER TABLE tasks ADD COLUMN owner_ms INT DEFAULT '0'");
+    
+    $pdo->exec("
+        CREATE TABLE groups (
+            id SERIAL PRIMARY KEY,
+            external_id VARCHAR(255) DEFAULT '',
+            name TEXT NOT NULL UNIQUE
+        )
+    ");
+    
+    $pdo->exec("
+        CREATE TABLE group_has_users (
+            group_id INTEGER NOT NULL,
+            user_id INTEGER NOT NULL,
+            FOREIGN KEY(group_id) REFERENCES groups(id) ON DELETE CASCADE,
+            FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+            UNIQUE(group_id, user_id)
+        )
+    ");
+}
+
+function version_1(PDO $pdo)
+{
     $pdo->exec("ALTER TABLE tasks ADD COLUMN owner_gp INT DEFAULT '0'");
 }
