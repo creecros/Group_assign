bc545ce134db6944085cd2ac18884f0f39a54d93
Craig Crosby <creecros@gmail.com>
2018-12-05 21:25:14 -0500

    Update SmallAvatarHelperExtend.php

--- Helper/SmallAvatarHelperExtend.php
+++ Helper/SmallAvatarHelperExtend.php
@@ -18,7 +18,17 @@ class SmallAvatarHelperExtend extends AvatarHelper
         $html = "";
         foreach ($assignees as $assignee) {
             $user = $this->userModel->getById($assignee['user_id']);
-            $html .= $this->render($assignee['user_id'], $user['username'], $user['name'], $user['email'], $user['avatar_path'], $css, 12);
+            $html .= $this->render($assignee['user_id'], $user['username'], $user['name'], $user['email'], $user['avatar_path'], $css, 20);
+        }
+        return $html;
+    }    
+    
+    public function miniMultiple($owner_ms, $css = '') {
+        $assignees = $this->multiselectMemberModel->getMembers($owner_ms);
+        $html = "";
+        foreach ($assignees as $assignee) {
+            $user = $this->userModel->getById($assignee['user_id']);
+            $html .= $this->render($assignee['user_id'], $user['username'], $user['name'], $user['email'], $user['avatar_path'], $css, 13);
         }
         return $html;
     }
