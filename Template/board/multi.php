a3dd8f481fedcf8a3c1b34a8e44ec019f105233c
Craig Crosby <creecros@gmail.com>
2018-12-05 21:30:50 -0500

    Update multi.php

--- Template/board/multi.php
+++ Template/board/multi.php
@@ -1,4 +1,4 @@
 <?php if ($task['owner_ms'] > 0 && count($this->task->multiselectMemberModel->getMembers($task['owner_ms'])) > 0) : ?>
       <strong><?= t('Other Assignees:') ?></strong>
-    <?= $this->helper->smallAvatarHelperExtend->smallMultiple($task['owner_ms'], 'avatar-inline') ?>
+    <?= $this->helper->smallAvatarHelperExtend->miniMultiple($task['owner_ms'], 'avatar-inline') ?>
 <?php endif ?>
