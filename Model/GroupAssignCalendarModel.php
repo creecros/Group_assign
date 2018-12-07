a346ebbb0d52b1d30f990eb1ed32432ae3ecfde8
Craig Crosby <creecros@gmail.com>
2018-12-07 09:54:49 -0500

    Update GroupAssignCalendarModel.php

--- Model/GroupAssignCalendarModel.php
+++ Model/GroupAssignCalendarModel.php
@@ -31,8 +31,19 @@ class GroupAssignCalendarModel extends Base
      */
     public function getUserCalendarEvents($user_id, $start, $end)
     {
-         $tasks = $this->db->table(self::TABLE)
+        $getMS_Ids = $this->db->table(MultiselectMemberModel::TABLE)
             ->eq('user_id', $user_id)
+            ->findAllByColumn('group_id');
+        
+        $getGr_Ids = $this->db->table(GroupMemberModel::TABLE)
+            ->eq('user_id', $user_id)
+            ->findAllByColumn('group_id');
+                
+         $tasks = $this->db->table(self::TABLE)
+            ->beginOr()
+            ->eq('owner_gp', $getGr_Ids)
+            ->eq('owner_ms', $getMS_Ids)
+            ->closeOr()
             ->gte('date_due', strtotime($start))
             ->lte('date_due', strtotime($end))
             ->neq('is_active', 0)
@@ -70,56 +81,5 @@ class GroupAssignCalendarModel extends Base
          
          return $events;
     }
-    
-    public function getProjectCalendarEvents($project_id, $start, $end)
-    {
-        $alltasks = $this->taskFinderModel->getAllIds($project_id);
-        
-        $tasks = array();
-        
-        foreach ($alltasks as $lonetask) {
-        
-         $foundtasks = $this->db->table(self::TABLE)
-            ->eq('task_id', $lonetask['id'])
-            ->gte('date_due', strtotime($start))
-            ->lte('date_due', strtotime($end))
-            ->neq('is_active', 0)
-            ->findAll();
-                   
-         $tasks = array_merge($tasks, $foundtasks);
-                        
-        }
-            
-         $events = array();
 
-         foreach ($tasks as $task) {
-                  
-         $startDate = new DateTime();
-         $startDate->setTimestamp($task['date_started']);
-         
-         $endDate = new DateTime();
-         $endDate->setTimestamp($task['date_due']);
-         
-         if ($startDate == 0) { $startDate = $endDate; }
-         
-         $allDay = $startDate == $endDate && $endDate->format('Hi') == '0000';
-         $format = $allDay ? 'Y-m-d' : 'Y-m-d\TH:i:s';
-            
-            $events[] = array(
-                'timezoneParam' => $this->timezoneModel->getCurrentTimezone(),
-                'id' => $task['id'],
-                'title' => t('#%d', $task['id']).' '.$task['title'],
-                'backgroundColor' => $this->colorModel->getBackgroundColor($task['color_id']),
-                'borderColor' => $this->colorModel->getBorderColor($task['color_id']),
-                'textColor' => 'black',
-                'url' => $this->helper->url->to('TaskViewController', 'show', array('task_id' => $task['id'], 'project_id' => $task['project_id'])),
-                'start' => $startDate->format($format),
-                'end' => $endDate->format($format),
-                'editable' => $allDay,
-                'allday' => $allDay,
-            );
-         }
-         
-         return $events;
-    }
 }
