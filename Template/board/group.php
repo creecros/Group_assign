8899303d143ac639f74d32d56a24a152a5f3d955
Craig Crosby <creecros@gmail.com>
2018-12-12 17:14:29 -0500

    Update group.php

--- Template/board/group.php
+++ Template/board/group.php
@@ -1,7 +1,7 @@
 <span>
 <?php if ($task['assigned_groupname']): ?>
      <strong><?= t('Assigned Group:') ?></strong>
-     <?= $this->text->e($task['assigned_groupname'] ?: $task['owner_gp']) ?>
+     <span class="assigned-group"><?= $this->text->e($task['assigned_groupname'] ?: $task['owner_gp']) ?></span>
      <br>
 <?php endif ?>
 </span>
