c2413dd935a4096923a386f8fab33674b1ca19f7
Craig Crosby <creecros@gmail.com>
2018-12-13 10:28:29 -0500

    Update GroupColorExtension.php

--- Model/GroupColorExtension.php
+++ Model/GroupColorExtension.php
@@ -12,4 +12,17 @@ class GroupColorExtension extends GroupModel
     $code = substr($code, 0, 6);
     return $code;
   }
+  
+  public function getFontColor($hex) {
+    // returns brightness value from 0 to 255
+    // strip off any leading #
+    $hex = str_replace('#', '', $hex);
+
+    $c_r = hexdec(substr($hex, 0, 2));
+    $c_g = hexdec(substr($hex, 2, 2));
+    $c_b = hexdec(substr($hex, 4, 2));
+
+    $brightness = (($c_r * 299) + ($c_g * 587) + ($c_b * 114)) / 1000;
+    if ($brightness > 130) { return 'black'; } else { return 'white'; }
+  }
 }
