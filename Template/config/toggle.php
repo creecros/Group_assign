c29b72de3dc16862ec739a2e280fb6e241b8b68c
Craig Crosby <creecros@gmail.com>
2019-01-16 09:46:20 -0500

    Update toggle.php

--- Template/config/toggle.php
+++ Template/config/toggle.php
@@ -1,4 +1,4 @@
 <div class="panel">
-    <?= $this->form->radio('enable_pm_group_management', 'Enable Group Managment for Project Managers' , 1, isset($values['enable_pm_group_management'])&& $values['enable_pm_group_management']==1) ?>
-    <?= $this->form->radio('enable_pm_group_management', 'Disable Group Managment for Project Managers' , 2, isset($values['enable_pm_group_management'])&& $values['enable_pm_group_management']==2) ?>
+    <?= $this->form->radio('enable_am_group_management', 'Enable Group Managment for Application Managers' , 1, isset($values['enable_am_group_management'])&& $values['enable_am_group_management']==1) ?>
+    <?= $this->form->radio('enable_am_group_management', 'Disable Group Managment for Application Managers' , 2, isset($values['enable_am_group_management'])&& $values['enable_am_group_management']==2) ?>
 </div>
