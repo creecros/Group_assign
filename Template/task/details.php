27cdbcd2bc274f914887163bca0e0daa46f9aa49
Cyboulette <cyboulette58@gmail.com>
2019-01-18 11:40:56 +0100

    Missing a quote

--- Template/task/details.php
+++ Template/task/details.php
@@ -2,7 +2,7 @@
                         <strong><?= t('Assigned Group:') ?></strong>
                         <span>
                         <?php if ($task['assigned_groupname']): ?>
-                            <span class="assigned-group" style="background-color: #<?= $this->task->groupColorExtension->getGroupColor($task['assigned_groupname']) ?>; color:"<?= $this->task->groupColorExtension->getFontColor($this->task->groupColorExtension->getGroupColor($task['assigned_groupname'])) ?>;"><?= $this->text->e($task['assigned_groupname'] ?: $task['owner_gp']) ?></span>
+                            <span class="assigned-group" style="background-color: #<?= $this->task->groupColorExtension->getGroupColor($task['assigned_groupname']) ?>; color:<?= $this->task->groupColorExtension->getFontColor($this->task->groupColorExtension->getGroupColor($task['assigned_groupname'])) ?>;"><?= $this->text->e($task['assigned_groupname'] ?: $task['owner_gp']) ?></span>
                         <?php else: ?>
                             <?= t('not assigned') ?>
                         <?php endif ?>
