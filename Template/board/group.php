fdfc476697934fbc79a7d1791e05ea6804f853c1
Cyboulette <cyboulette58@gmail.com>
2019-01-18 11:34:01 +0100

    Fix css when displaying groups
    Typo issue, the style tag was closed before color, before this "fix" it rendered like that
    <span class="assigned-group" style="background-color: #000000" color:"#ff3fe4">MyGroup</span>

--- Template/board/group.php
+++ Template/board/group.php
@@ -1,7 +1,7 @@
 <span>
 <?php if ($task['assigned_groupname']): ?>
      <strong><?= t('Assigned Group:') ?></strong>
-     <span class="assigned-group" style="background-color: #<?= $this->task->groupColorExtension->getGroupColor($task['assigned_groupname']) ?>" color:"<?= $this->task->groupColorExtension->getFontColor($this->task->groupColorExtension->getGroupColor($task['assigned_groupname'])) ?>"><?= $this->text->e($task['assigned_groupname'] ?: $task['owner_gp']) ?></span>
+     <span class="assigned-group" style="background-color: #<?= $this->task->groupColorExtension->getGroupColor($task['assigned_groupname']) ?>; color:"<?= $this->task->groupColorExtension->getFontColor($this->task->groupColorExtension->getGroupColor($task['assigned_groupname'])) ?>;"><?= $this->text->e($task['assigned_groupname'] ?: $task['owner_gp']) ?></span>
      <br>
 <?php endif ?>
 </span>
