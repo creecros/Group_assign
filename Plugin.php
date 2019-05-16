ca828eee676a8c40e489afec661b28e0f580baff
creecros <creecros@gmail.com>
2019-05-16 15:52:45 +0000

    plugin version

--- Plugin.php
+++ Plugin.php
@@ -196,7 +196,7 @@ class Plugin extends Base
     }
     public function getPluginVersion()
     {
-        return '1.6.2';
+        return '1.7.0';
     }
     public function getPluginHomepage()
     {
