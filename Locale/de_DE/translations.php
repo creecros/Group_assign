d688b798b878d992386f977aa9cf0428c408fe07
Timo <timo.litzbarski@gmail.com>
2019-07-29 16:48:52 +0200

    german translation

---
+++ Locale/de_DE/translations.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+		'Assigned Group' => 'Zugeordnete Gruppe',
+    'Assigned Group:' => 'Zugeordnete Gruppe:',
+    'Other Assignees' => 'Weitere Zuordnungen',
+    'Other Assignees:' => 'Weitere Zuordnungen:',
+    'Send a task by email to assigned group members' => 'Senden einer Aufgabe per E-Mail an zugewiesene Gruppenmitglieder',
+		'Send a task by email to the other assignees for a task.' => 'Aufgabe per E-Mail an die weiteren zugeordneten der Aufgabe senden',
+		'Send email notification of impending due date to Group Members Assigned' => 'E-Mail-Benachrichtigung über bevorstehendes Fälligkeitsdatum an die zugewiesenen Gruppenmitglieder senden',
+		'Send email notification of impending due date to the other assignees of a task' => 'E-Mail-Benachrichtigung über das bevorstehende Fälligkeitsdatum an die anderen Empfänger einer Aufgabe senden',
+    'Assign the task to a specific group' => 'Aufgabe einer bestimmten Gruppe zuordnen'
+);
