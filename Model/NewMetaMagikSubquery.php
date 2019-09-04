9afc517aa3e76291ccc7c370cfd4680449ec7a3e
root <creecros@gmail.com>
2019-09-04 15:56:36 +0000

    Compatibility with MetaMagik latest version

---
+++ Model/NewMetaMagikSubquery.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kanboard\Plugin\Group_assign\Model;
+
+use Kanboard\Plugin\Group_assign\Model\NewTaskFinderModel;
+
+/**
+ * New Task Finder model
+ * Extends Group_assign Model
+ *
+ * @package  Kanboard\Plugin\Group_assign\Model
+ */
+class NewMetaMagikSubQuery extends NewTaskFinderModel
+{
+    const METADATA_TABLE = 'task_has_metadata';
+    /**
+     * Extended query
+     *
+     * @access public
+     * @return \PicoDb\Table
+     */
+    public function getExtendedQuery()
+    {
+        // add subquery to original Model, changing only what we want
+        return parent::getExtendedQuery()
+            ->subquery('(SELECT COUNT(*) FROM '.self::METADATA_TABLE.' WHERE task_id=tasks.id)', 'nb_metadata');
+    }
+}
