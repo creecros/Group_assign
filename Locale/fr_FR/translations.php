03432aad98b44ccc77cf7352d77c8ce5142aadd8
Chtiland <fabrice@chtiland.com>
2021-02-19 18:19:40 +0100

    French translation

---
+++ Locale/fr_FR/translations.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'Assigned Group' => 'Groupe assigné',
+	'Assigned Group:' => 'Groupe assigné :',
+	'Other Assignees' => 'Autres assignés',
+	'Other Assignees:' => 'Autres assignés :',
+	'Send a task by email to assigned group members' => 'Envoyer une tâche par e-mail aux membres du groupe assigné',
+	'Send a task by email to the other assignees for a task.' => 'Envoyer une tâche par e-mail aux autres personnes assignées à la tâche',
+	'Send email notification of impending due date to Group Members Assigned' => 'Envoyer un e-mail de notification à l\'approche d\'une date d\'échéance aux membres du groupe assigné',
+	'Send email notification of impending due date to the other assignees of a task' => 'Envoyer un e-mail de notification à l\'approche d\'une date d\'échéance aux autres personnes assignées à la tâche',
+	'Assign the task to a specific group' => 'Assigner la tâche à un groupe spécifique'
+);
