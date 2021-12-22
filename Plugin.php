957fd95b8483211825207b5159690d8230a4c6dd
Jieiku <106644+Jieiku@users.noreply.github.com>
2021-12-22 07:02:57 -0800

    Compact Layout

--- Plugin.php
+++ Plugin.php
@@ -31,12 +31,11 @@ use Kanboard\Core\Security\Role;
 
 class Plugin extends Base
 {
-    
     public function initialize()
     {
-        //Events & Changes        
+        //Events & Changes
         $this->template->setTemplateOverride('task/changes', 'group_assign:task/changes');
-        
+
         //Notifications
         $this->container['userNotificationFilterModel'] = $this->container->factory(function ($c) {
                 return new NewUserNotificationFilterModel($c);
@@ -44,15 +43,14 @@ class Plugin extends Base
 
         //Helpers
         $this->helper->register('newTaskHelper', '\Kanboard\Plugin\Group_assign\Helper\NewTaskHelper');
-        $this->helper->register('smallAvatarHelperExtend', '\Kanboard\Plugin\Group_assign\Helper\SmallAvatarHelperExtend');
-        
-        
+        $this->helper->register('sizeAvatarHelperExtend', '\Kanboard\Plugin\Group_assign\Helper\SizeAvatarHelperExtend');
+
+
         //Models and backward compatibility
-        
         $applications_version = str_replace('v', '', APP_VERSION);
         if (strpos(APP_VERSION, 'master') !== false && file_exists('ChangeLog')) { $applications_version = trim(file_get_contents('ChangeLog', false, null, 8, 6), ' '); }
         $clean_appversion = preg_replace('/\s+/', '', $applications_version);
-        
+
         if (version_compare($clean_appversion, '1.2.5', '>')) {
             if (file_exists('plugins/MetaMagik')){
                 $this->container['taskFinderModel'] = $this->container->factory(function ($c) {
@@ -98,18 +96,23 @@ class Plugin extends Base
                 return new TaskRecurrenceModel ($c);
             });
         }
-        
+
         //Task - Template - details.php
         $this->template->hook->attach('template:task:details:third-column', 'group_assign:task/details');
         $this->template->hook->attach('template:task:details:third-column', 'group_assign:task/multi');
-        
+
         //Forms - task_creation.php and task_modification.php
         $this->template->setTemplateOverride('task_creation/show', 'group_assign:task_creation/show');
         $this->template->setTemplateOverride('task_modification/show', 'group_assign:task_modification/show');
-        
+
         //Board
         $this->template->hook->attach('template:board:private:task:before-title', 'group_assign:board/group');
-        $this->template->hook->attach('template:board:private:task:before-title', 'group_assign:board/multi');
+
+        if ($this->configModel->get('boardcustomizer_compactlayout', '') == 'enable') {
+            $this->template->hook->attach('template:board:private:task:before-avatar', 'group_assign:board/multi');
+        } else {
+            $this->template->hook->attach('template:board:private:task:before-title', 'group_assign:board/multi');
+        }
         $groupmodel = $this->projectGroupRoleModel;
         $this->template->hook->attachCallable('template:app:filters-helper:after', 'group_assign:board/filter', function($array = array()) use ($groupmodel) {
             if(!empty($array) && $array['id'] >= 1){
@@ -117,25 +120,25 @@ class Plugin extends Base
             } else {
                 return ['grouplist' => array()];
             }
-        });  
-        
+        });
+
         //Filter
         $this->container->extend('taskLexer', function($taskLexer, $c) {
             $taskLexer->withFilter(TaskAllAssigneeFilter::getInstance()->setDatabase($c['db'])
                                                                     ->setCurrentUserId($c['userSession']->getId()));
             return $taskLexer;
         });
-        
+
         //Actions
         $this->actionManager->register(new EmailGroup($this->container));
         $this->actionManager->register(new EmailGroupDue($this->container));
         $this->actionManager->register(new EmailOtherAssignees($this->container));
         $this->actionManager->register(new EmailOtherAssigneesDue($this->container));
         $this->actionManager->register(new AssignGroup($this->container));
-        
+
         //Params
         $this->template->setTemplateOverride('action_creation/params', 'group_assign:action_creation/params');
-        
+
         //CSS
         $this->hook->on('template:layout:css', array('template' => 'plugins/Group_assign/Assets/css/group_assign.css'));
 
@@ -144,16 +147,16 @@ class Plugin extends Base
 
         //Calendar Events
         $container = $this->container;
-        
+
         $this->hook->on('controller:calendar:user:events', function($user_id, $start, $end) use ($container) {
             $model = new GroupAssignCalendarModel($container);
             return $model->getUserCalendarEvents($user_id, $start, $end); // Return new events
         });
-        
+
         //Roles
 
         $this->template->hook->attach('template:config:application', 'group_assign:config/toggle');
-    
+
         if ($this->configModel->get('enable_am_group_management', '2') == 1) { 
             $this->applicationAccessMap->add('GroupListController', '*', Role::APP_MANAGER);
             $this->applicationAccessMap->add('GroupCreationController', '*', Role::APP_MANAGER);
@@ -162,12 +165,12 @@ class Plugin extends Base
 
 
     }
-    
+
     public function onStartup()
     {
         Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
     }
-    
+
     public function getClasses()
     {
         return [
--- Template/board/multi.php
+++ Template/board/multi.php
@@ -1,5 +1,22 @@
 <?php if ($task['owner_ms'] > 0 && count($this->task->multiselectMemberModel->getMembers($task['owner_ms'])) > 0) : ?>
-<strong class="assigned-other-label"><small><?= t('Other Assignees:') ?></small></strong>
-    <?= $this->helper->smallAvatarHelperExtend->miniMultiple($task['owner_ms'], 'avatar-inline') ?>
-<br>
+
+<?php if ($this->user->userMetadataModel->exists($this->user->getid(), "boardcustomizer_compactlayout")) {
+    /* compact card layout */
+    ?>
+
+    <strong class="assigned-other-label"><small><?= t('Other Assignees:') ?></small></strong>
+    <?= $this->helper->sizeAvatarHelperExtend->sizeMultiple($task['owner_ms'], 'avatar-inline avatar-bdyn', $this->task->configModel->get('b_av_size','35')) ?>
+
+<?php
+} else {
+?>
+
+    <strong class="assigned-other-label"><small><?= t('Other Assignees:') ?></small></strong>
+    <?= $this->helper->sizeAvatarHelperExtend->sizeMultiple($task['owner_ms'], 'avatar-inline avatar-bdyn', 13) ?>
+    <br>
+
+<?php
+}
+?>
+
 <?php endif ?>
