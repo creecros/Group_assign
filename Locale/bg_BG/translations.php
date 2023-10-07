00f5ffc80ae880880b6f8abc05bc243c31883e81
Bogdan Marinski <bogdan.marinski@gmail.com>
2023-10-07 14:04:41 +0300

    Add Bulgarian translation

---
+++ Locale/bg_BG/translations.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'Assigned Group' => 'Изпълнителна група',
+    'Assigned Group:' => 'Изпълнителна група:',
+    'Other Assignees' => 'Други изпълнители',
+    'Other Assignees:' => 'Други изпълнители:',
+    'Send a task by email to assigned group members' => 'Изпращане на задача по имейл до определената изпълнителна група',
+    'Send a task by email to the other assignees for a task.' => 'Изпращане на задача по имейл до другите изпълнители на задачата',
+    'Send email notification of impending due date to Group Members Assigned' => 'Изпращане на имейл известие за изтичащ краен срок до избраните членове на група',
+    'Send email notification of impending due date to the other assignees of a task' => 'Изпращане на имейл известие за изтичащ краен срок до другите изпълнители на задача',
+    'Assign the task to a specific group' => 'Присвои задачата към специфична група'
+);
